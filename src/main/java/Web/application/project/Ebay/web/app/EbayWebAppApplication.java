8babafc24d8098df70b14a996a5706462e99ebf8
landmark <nnamdiiwenofu@yahoo.co.uk>
2023-09-19 09:31:51 -0500

    my ebay web app

---
+++ src/main/java/Web/application/project/Ebay/web/app/EbayWebAppApplication.java
@@ -0,0 +1,13 @@
+package Web.application.project.Ebay.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Web/application/project/Ebay/web/app/EbayWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Web.application.project.Ebay.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbayWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
